804db7172c68fcb0095904d511f6f09d37ca01af
Anthony <huasipango@gmail.com>
2019-07-07 12:01:09 -0500

    Hotfix de ejercicio de Jipson.

---
+++ src/main/java/com/medico/app/web/api/MedicamentoApiController.java
@@ -0,0 +1,37 @@
+package com.medico.app.web.api;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.medico.app.web.models.entities.Medicamento;
+import com.medico.app.web.models.entities.Medico;
+import com.medico.app.web.models.services.IMedicamentoService;
+
+@RestController
+@RequestMapping(value="/api")
+public class MedicamentoApiController {
+	 @Autowired
+	    private IMedicamentoService service;
+
+	    @GetMapping("/medicamento")
+	    public List<Medicamento> list() {
+	        return service.findByAll();
+	    }
+	    @GetMapping("/medicameto-paciente/{id}")
+	    public List<Medicamento> listMedicamentoByPaciente(@PathVariable String id) {
+	        return service.listMedicamentoByPaciente(id);
+	    }
+	   
+	    @GetMapping(value = "/find/{criteria}", produces = { "application/json" })
+		public @ResponseBody List<Medicamento> findByNombre(@PathVariable String criteria) {
+			return service.listMedicamentoByPaciente(criteria);
+		}
+	    
+}
